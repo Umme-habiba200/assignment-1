8867f48968186d153a05229e4d65f7911992e94c
Umme-habiba200 <habiba.riaz991@gmail.com>
2024-10-30 16:22:07 +0500

    Add files via upload

---
+++ habiba.py
@@ -0,0 +1,28 @@
+a=8 #num1
+b=6#num2
+c=2 #num
+s=a+b+c #sum
+print(s)
+d=a-b-c #difference
+print(d)
+avg=s/3 #average
+print(a)
+p=a*b*c #product
+print(p)
+B=10 #base in cm
+H=20 #height in cm
+A=1/2*B*H #area of tringle in cm^2
+print(A)
+s1=9 #side of square in cm
+S=s1*s1 #area of square in cm^2
+print(S)
+r=12 #radius of circle in cm
+c=22/7*r**2 #area of circle cm^2
+print(c)
+l=5 #length of a rectangle in cm
+w=13 #width of a rectangle in cm
+R=l*w #area of rectangle cm^2
+print(R)
+
+
+
